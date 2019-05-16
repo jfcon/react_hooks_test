7c9be50a2e92dd44d7a11628a8e19fd37d0bb98e
John <connollyjohnf@gmail.com>
2019-05-16 21:04:44 +0000

    turned function into a class that has state. It's more code, and major rearranging is involved.

--- test.js
+++ test.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-function OneTimeButton(props){
+class OneTimeButton extends React.Component {
+    state ={
+        clicked: false
+    }
+
+
+//click handler
+handleClick = () => {
+    // click handler won't be called if the button 
+    // is disabled, so if we got here, it's safe to 
+    // trigger the click
+    this.props.onClick();
+    this.ListeningStateChangedEvent({clicked: true});
+}
+
+render() {
     return (
-        <button onClick={props.onClick}>
-            You can only click me once
+        <button
+        onClick = {this.handleClick}
+        disabled={this.state.clicked}
+        >
+            You can only click me once...
         </button>
-    )
+        );
+    }
 }
-
-function sayHi(){
-    console.log('Heya!');
-}
-
-render(
-    <OneTimeButton onClick={sayHi}/>,
-    document.querySelector('#root')
-)
